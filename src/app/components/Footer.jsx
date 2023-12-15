ef8d881a270dff1d45b1eeeec4556e3da196bf0a
Asilbek Muminov <asilbekmuminov@Asilbeks-MacBook-Air.local>
2023-12-15 20:58:16 +0100

    fix: small footer fix

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Socials } from "./Socials";
 
 export const Footer = () => {
   return (
-    <div className="self-center flex w-full max-w-[1199px] justify-center md:justify-between gap-5 md:my-9 max-md:max-w-full max-md:flex-wrap">
+    <div className="self-center flex w-full max-w-[1199px] justify-center md:justify-between gap-5 my-9 max-md:max-w-full max-md:flex-wrap">
       <div className="flex items-start justify-between gap-5 mt-2.5 px-5 max-md:justify-center">
         <div className="text-red-800 text-base font-medium leading-5 self-start">
           Home
